2fb44ee86e25c66f020692951224c4871b7af95b
Diego Carciente <diego.carciente@gmail.com>
2017-07-24 15:58:26 -0400

    creando el footer del thema

--- wp-content/themes/lapizzeria/footer.php
+++ wp-content/themes/lapizzeria/footer.php
@@ -1,4 +1,23 @@
 
+  <footer>
+    <?php
+      $args = array(
+        'theme_location' => 'header-menu',
+        'container' => 'nav',
+        'after' => '<span class="separator"> | </span>'
+      );
+
+      wp_nav_menu( $args );
+    ?>
+
+    <div class="ubicacion">
+      <p>8179 Bay Avenue Mountain View, CA 94043</p>
+      <p>Telefono: +1-92-456-7890</p>
+    </div>
+
+    <p class="copyright"> Todos los derechos reservados <?php echo date('Y') ?></p>
+  </footer>
+
   <?php wp_footer(); ?>
   </body>
 </html>
