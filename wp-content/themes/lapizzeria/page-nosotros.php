44a3795859c8b3b9a19a7e0d2079e136ba9ea176
Diego Carciente <diego.carciente@gmail.com>
2017-07-24 16:59:54 -0400

    agregando plugin de resize mostrando los CUSTOM FIELDS

--- wp-content/themes/lapizzeria/page-nosotros.php
+++ wp-content/themes/lapizzeria/page-nosotros.php
@@ -19,7 +19,54 @@
       </main>
     </div>
 
-    <h2>Este es el page-nosotros.php</h2>
+    <div class="informacion-cajas contenedor">
+
+      <pre><?php echo get_field('color1') ?></pre>
+
+      <div class="cajas">
+        <?php
+         $id_imagen  = get_field('imagen');
+         $imagen = wp_get_attachment_image_src( $id_imagen, $size = 'nosotros' )
+        ?>
+
+        <img src="<?php echo $imagen[0] ?>" class="caja-imagen">
+
+        <div class="contenido-caja" style="background-color: <?php echo get_field('color1') ?>">
+          <?php the_field('descripcion') ?>
+        </div>
+      </div>
+
+
+      <div class="cajas">
+        <div class="contenido-caja" style="background-color: <?php echo get_field('color1') ?>">
+          <?php the_field('descripcion2') ?>
+        </div>
+
+        <?php
+         $id_imagen  = get_field('imagen2');
+         $imagen = wp_get_attachment_image_src( $id_imagen, $size = 'nosotros' )
+        ?>
+
+        <img src="<?php echo $imagen[0] ?>" class="caja-imagen">
+      </div>
+
+
+      <div class="cajas">
+        <?php
+         $id_imagen  = get_field('imagen3');
+         $imagen = wp_get_attachment_image_src( $id_imagen, $size = 'nosotros' )
+        ?>
+
+        <img src="<?php echo $imagen[0] ?>" class="caja-imagen">
+
+        <div class="contenido-caja" style="background-color: <?php echo get_field('color1') ?>">
+          <?php the_field('descripcion3') ?>
+        </div>
+      </div>
+
+
+
+    </div>
 
   <?php endwhile; ?>
 
