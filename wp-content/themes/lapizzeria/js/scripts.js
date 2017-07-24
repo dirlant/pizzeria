1ba77e523733617704a11eeeafe0073d547432af
Diego Carciente <diego.carciente@gmail.com>
2017-07-24 12:41:49 -0400

    agregando comportamiento del menu responsive

--- wp-content/themes/lapizzeria/js/scripts.js
+++ wp-content/themes/lapizzeria/js/scripts.js
@@ -1,5 +1,22 @@
 $ = jQuery.noConflict();
 
 $(document).ready(function(){
-  alert('hola mundo')
+
+  // Ocultar y mostrar menu
+  $('.mobile-menu a').on('click', function(event) {
+    $('nav.menu-sitio').toggle('slow');
+
+  });
+
+
+  // Reaccionar a la dimensión de pantalla
+  $(window).resize(function() {
+    console.log($(document).width())
+    if ($(window).width() >= 768 ){
+      $('nav.menu-sitio').show();
+    } else {
+      $('nav.menu-sitio').hide();
+    }
+  });
+
 })
