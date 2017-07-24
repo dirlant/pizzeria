208fb341b5ed5b64506d5bc23c8803d71379c0ee
Diego Carciente <diego.carciente@gmail.com>
2017-07-24 13:05:59 -0400

    agregando imagen destacada, titulo, post

--- wp-content/themes/lapizzeria/page.php
+++ wp-content/themes/lapizzeria/page.php
@@ -1,5 +1,17 @@
 <?php get_header() ?>
 
-    <h1>Hola Mundo desde el page.php</h1>
+  <?php while (have_posts()): ?>
+
+    <?php the_post(); ?>
+    <?php the_post_thumbnail() ?>
+    <?php the_title( $before = '<h1>', $after = '</h1>', $echo = true ) ?>
+
+    <div class="principal contenedor">
+      <main>
+        <?php the_content() ?>
+      </main>
+    </div>
+
+  <?php endwhile; ?>
 
 <?php get_footer() ?>
