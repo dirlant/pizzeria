56592045da7c694082d74e7515a92a761f5cf10c
Diego Carciente <diego.carciente@gmail.com>
2017-07-25 14:06:08 -0400

    mostrando las entradas del blog

--- wp-content/themes/lapizzeria/index.php
+++ wp-content/themes/lapizzeria/index.php
@@ -1,5 +1,38 @@
 <?php get_header() ?>
 
-    <h1>Hola Mundo desde el index.php</h1>
+  <?php
+
+    $pagina_blog =  get_option( 'page_for_posts', $default = true );
+    $img = get_post_thumbnail_id( $pagina_blog );
+    $img = wp_get_attachment_image_src( $img, $size = 'full', $icon = false )
+
+  ?>
+
+  <div class="hero" style="background-image: url(<?php echo $img[0];   ?>)">
+    <div class="contenido-hero">
+      <div class="texto-hero">
+        <?php get_the_title( $post = $pagina_blog ) ?>
+        <?php the_title( $before = '<h1>', $after = '</h1>', $echo = true ) ?>
+      </div>
+    </div>
+  </div>
+
+
+
+    <div class="principal contenedor">
+      <main class="texto-centrado contenido-paginas">
+        <?php while (have_posts()): the_post(); ?>
+
+          <article class="entrada-blog">
+            <?php the_title(); ?>
+          </article>
+
+        <?php endwhile; ?>
+      </main>
+    </div>
+
+
+
+
 
 <?php get_footer() ?>
