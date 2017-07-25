645d2c10d1d999fe1656d7962ee2a01c906766d3
Diego Carciente <diego.carciente@gmail.com>
2017-07-25 17:40:52 -0400

    agregando formulario de comentarios

--- wp-content/themes/lapizzeria/single.php
+++ wp-content/themes/lapizzeria/single.php
@@ -1,5 +1,30 @@
 <?php get_header() ?>
 
-    <h1>Hola Mundo desde el single.php</h1>
+  <?php while (have_posts()): the_post(); ?>
+
+
+    <div class="hero" style="background-image: url(<?php echo get_the_post_thumbnail_url()   ?>)">
+      <div class="contenido-hero">
+        <div class="texto-hero">
+          <?php the_title( $before = '<h1>', $after = '</h1>', $echo = true ) ?>
+        </div>
+      </div>
+    </div>
+
+
+
+    <div class="principal contenedor">
+      <main class="texto-centrado contenido-paginas">
+        <?php the_content() ?>
+      </main>
+    </div>
+
+  <?php endwhile; ?>
+
+  <div class="contenedor comentarios">
+
+    <?php comment_form() ?>
+
+  </div>
 
 <?php get_footer() ?>
