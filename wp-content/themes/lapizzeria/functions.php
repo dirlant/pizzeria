e43fcc1a8d818cb262fd84c453d23fbe49f2184f
Diego Carciente <diego.carciente@gmail.com>
2017-07-26 10:56:48 -0400

    agregando libreria fluidbox para galeria

--- wp-content/themes/lapizzeria/functions.php
+++ wp-content/themes/lapizzeria/functions.php
@@ -14,20 +14,24 @@ function lapizzeria_styles(){
   wp_register_style('normalize', $src = get_template_directory_uri() . '/css/normalize.css', $deps = array(), $ver = 'v7.0.0' );
   wp_register_style('google_fonts', $src = 'https://fonts.googleapis.com/css?family=Open+Sans|Raleway:400,700,900', $deps = array(), $ver = 'v1.0.0');
   wp_register_style('fontawesome', $src = get_template_directory_uri() . '/css/font-awesome.min.css', $deps = array('normalize'), $ver = 'v4.7.0' );
+  wp_register_style('fluidbox', $src = get_template_directory_uri() . '/librerias/fluidbox/css/fluidbox.min.css', $deps = array(), $ver = 'v2.0' );
   wp_register_style('style', $src = get_template_directory_uri() . '/style.css', $deps = array('normalize'), $ver = 'v1.0' );
 
   // Llamando a los estilos
   wp_enqueue_style('normalize');
   wp_enqueue_style('google_fonts');
   wp_enqueue_style('fontawesome');
+  wp_enqueue_style('fluidbox');
   wp_enqueue_style('style');
 
   // Registrando JS
   wp_register_script('scripts', $src = get_template_directory_uri() . '/js/scripts.js', $deps = array(), $ver = 'v1.0.0', $in_footer = true );
+  wp_register_script('fluidbox', $src = get_template_directory_uri() . '/librerias/fluidbox/js/jquery.fluidbox.min.js', $deps = array(), $ver = 'v1.0.0', $in_footer = true );
 
   // Llamando a los scripts
   wp_enqueue_script('jquery');
   wp_enqueue_script('scripts');
+  wp_enqueue_script('fluidbox');
 }
 
 add_action( 'wp_enqueue_scripts', 'lapizzeria_styles');
