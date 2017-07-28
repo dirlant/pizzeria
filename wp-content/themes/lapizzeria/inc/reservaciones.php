d351392ff1b219d669fd8991e5a21dc6ad0340d2
Diego Carciente <diego.carciente@gmail.com>
2017-07-28 17:23:36 -0400

    insertando datos en la base de datos

--- wp-content/themes/lapizzeria/inc/reservaciones.php
+++ wp-content/themes/lapizzeria/inc/reservaciones.php
@@ -1,14 +1,37 @@
 <?php
 
 function lapizzeria_guardar(){
+  global $wpdb;
+  $tabla = $wpdb->prefix . 'reservaciones';
+
   if (isset($_POST['enviar'])):
-    $nombre= sanitize_text_field( $_POST['nombre']);
-    $nombre= sanitize_text_field( $_POST['fecha']);
-    $nombre= sanitize_text_field( $_POST['correo']);
-    $nombre= sanitize_text_field( $_POST['telefono']);
-    $nombre= sanitize_text_field( $_POST['mensaje']);
-    echo $nombre;
+
+    $nombre = sanitize_text_field( $_POST['nombre']);
+    $fecha = sanitize_text_field( $_POST['fecha']);
+    $correo = sanitize_text_field( $_POST['correo']);
+    $telefono = sanitize_text_field( $_POST['telefono']);
+    $mensaje = sanitize_text_field( $_POST['mensaje']);
+
+    $datos = array(
+      'nombre' => $nombre,
+      'fecha' => $fecha,
+      'correo' => $correo,
+      'telefono' => $telefono,
+      'mensaje' => $mensaje
+    );
+
+    $formato = array(
+      '%s',
+      '%s',
+      '%s',
+      '%s',
+      '%s'
+    );
+
+    $wpdb->insert($tabla, $datos, $formato);
+
   endif;
+
 }
 
 add_action( 'init', 'lapizzeria_guardar' );
